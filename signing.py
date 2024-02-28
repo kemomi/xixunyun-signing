a455414c4e1f8774452b3e7f2a0e80f37ad5ddd4
jy7730 <151915086+jy7730@users.noreply.github.com>
2024-02-29 00:12:26 +0800

    Update signing.py

--- signing.py
+++ signing.py
@@ -5,16 +5,16 @@ import time
 
 #习讯云签到脚本
    
-data={'account':'192210418',#账号
+data={'account':'21092030121',#账号
       'app_id':'cn.vanber.xixunyun.saas',
       'app_version':'4.1.5',
       'key':'',
       'model':'SM-G955N',
-      'password':'210418',#密码
+      'password':'wj030121',#密码
       'platform':'2',
       'registration_id':'160a3797c8437218079',
       'request_source':'3',
-      'school_id':'313',#学校代码
+      'school_id':'7',#学校代码
       'system':'4.4.2',
       'uuid':'48:45:20:B9:D7:19'}
 login_header={
@@ -35,8 +35,8 @@ latitude='33.623806'#维度
 longitude='119.018205'#经度
 print(login_data)
 sign_url='https://api.xixunyun.com/signin?token='+token+'&from=app&version=4.1.5&platform=android&entrance_year=0&graduate_year=0 '
-sign_data={'address':'',#签到地址
-           'address_name':'',#签到地点名称
+sign_data={'address':'南京市栖霞区八卦洲街道鹂岛路268号八卦洲科创园B栋办公楼1-1039',#签到地址
+           'address_name':'南京优宠网络科技有限公司',#签到地点名称
            'change_sign_resource':'0',
            'comment':'',
            'latitude':latitude,
