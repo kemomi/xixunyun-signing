8bebd8b1caafeff70ee271296b89110c490cd54a
jy7730 <151915086+jy7730@users.noreply.github.com>
2024-02-29 00:18:21 +0800

    Update signing.py

--- signing.py
+++ signing.py
@@ -31,8 +31,8 @@ login_data=json.loads(request.text)#登陆成功后返回的信息
 token=login_data['data']['token']
 time.sleep(1)
 #经纬度地址
-latitude='33.623806'#维度
-longitude='119.018205'#经度
+latitude='32.17955'#维度
+longitude='118.79552'#经度
 print(login_data)
 sign_url='https://api.xixunyun.com/signin?token='+token+'&from=app&version=4.1.5&platform=android&entrance_year=0&graduate_year=0 '
 sign_data={'address':'南京市栖霞区八卦洲街道鹂岛路268号八卦洲科创园B栋办公楼1-1039',#签到地址
